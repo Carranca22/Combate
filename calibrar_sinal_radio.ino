48986a74634aedf5395c38a1cb342d9fab35cfc1
Marcus Vinícius de Souza Feitosa <marcusvinic96@gmail.com>
2017-06-09 14:40:52 -0300

    Create calibrar_sinal_radio.ino

--- calibrar_sinal_radio.ino
+++ calibrar_sinal_radio.ino
@@ -1,40 +1,7 @@
-/* Programacao Combate V2.2.4.ino
- * Código para controle de robô beetleweight
- * Autores: Isabella Galvão, Marcus Vinícius e Kewin Lima.
- * 16/03/2015
- */
-//Inclui biblioteca para acesso a funções matemáticas avançadas
-#include <math.h> 
-/*Define os pinos que os motores estarão conectados
-essas variaveis irão se alterar de acordo com o ângulo em Z */
-int MOTOR_E1 = 5;
-int MOTOR_E2 = 6;
-int MOTOR_D1 = 9;
-int MOTOR_D2 = 10;
-
-//Caso 1 a saida será pela serial caso 0 será a resposta para os motores
-#define DEBUG 1
-
-// [ 1 ]- Imprime a saida aile e ele, [ 2 ] - imprime a resposta para os motores, [ 3 ] - imprime ambos
-#define TIPO_DE_DEBUG 2
-
-//Define o numero de interaões do filtro das entradas do controle
-#define NUMERO_DE_INTERACOES 1
-
-//Define o delay de espera para a vizualização na porta serial
 #define DELAY 0
-
 //Define as entradas de sinal do controle
 #define PORTA_AILE A0
 #define PORTA_ELE A1
-
-//Constantes para leitura do controle (Devem ser calibradas)
-#define ENTRADA_MAX 1894
-#define ENTRADA_MIN 1102
-
-//Constantes para saida para os motores
-#define SAIDA_MAX 255
-#define SAIDA_MIN -255
 
 //Vão guardar as leituras vindas do cont
 void setup()
@@ -42,11 +9,6 @@ void setup()
   // put your setup code here, to run once:
   pinMode(PORTA_AILE, INPUT);
   pinMode(PORTA_ELE, INPUT);
-
-  pinMode(MOTOR_E1, OUTPUT);
-  pinMode(MOTOR_E2, OUTPUT);
-  pinMode(MOTOR_D1, OUTPUT);
-  pinMode(MOTOR_D2, OUTPUT);
   Serial.begin(9600);
   Serial.flush();
 }
@@ -57,5 +19,5 @@ void loop(){
  Serial.println(aile_sinal);
  Serial.print("Ele : ");
  Serial.println(ele_sinal);
- 
+ delay(DELAY);
 }
